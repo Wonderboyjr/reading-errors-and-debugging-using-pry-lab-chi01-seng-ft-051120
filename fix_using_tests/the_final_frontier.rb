fb1ac988ad364f982a89e401f97489839d169d3a
Jonathan Reid <iamjonathanreid@gmail.com>
2020-04-25 04:07:42 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -2,7 +2,7 @@ def generate_star_date
   star_date = (rand(100000) + 400000) / 10
 end
 
-def state_log(generate_star_dat
+def state_log(generate_star_date)
   "Captain's Log, star date #{star_date}."
 end
 
